651e36112853991f68586205dc35237e3dddb064
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-10-28 15:36:46 +0700

    Menambahkan endpoint GET /komik yang digunakan untuk mengambil semua data komik dari database

--- index.js
+++ index.js
@@ -29,3 +29,12 @@ app.post('/komik', async (req, res)=> {
     }
 });
 
+app.get('/komik', async (req, res) => {
+    try{
+        const komik = await db.Komik.findAll();
+        res.send(komik);
+    }  catch (error) {
+        res.status(500).send({message: error.message});
+    }
+});
+
